ffad1d50eeffacf3ca828c8634692fc39862af4d
gadgete <evanchittick@gmail.com>
2024-03-18 19:30:29 -0400

    Movement Finished

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float hInput;
+    private float vInput;
+    [SerializeField] private float jump;
+    [SerializeField] private float speed;
+
+    Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
     }
 }
