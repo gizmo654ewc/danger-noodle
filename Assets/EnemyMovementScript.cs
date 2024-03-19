b0c741bd94c0666ce7ff81dcbaf593a0baca08e9
gadgete <evanchittick@gmail.com>
2024-03-18 22:40:23 -0400

    First ladder pathing complete

---
+++ Assets/EnemyMovementScript.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementScript : MonoBehaviour
+{
+    //waypoints
+    [SerializeField] private GameObject spawnPoint;
+    [SerializeField] private GameObject wayPoint1_1;
+    [SerializeField] private GameObject wayPoint1_2;
+    //[SerializeField] private GameObject wayPoint2_1;
+    //[SerializeField] private GameObject wayPoint2_2;
+
+    Rigidbody2D rb;
+
+    public float speed;
+    public int currentPoint = 0;
+    private bool climbing = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = spawnPoint.transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(0, 1000));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentPoint == 0)
+        {
+            if (!climbing)
+            {
+                if (transform.position.x > wayPoint1_1.transform.position.x)
+                {
+                    rb.velocity = new Vector2(-speed, rb.velocity.y);
+                }
+                else
+                {
+                    rb.velocity = new Vector2(speed, rb.velocity.y);
+                }
+
+                if (wayPoint1_1.transform.position.x-.3f >= transform.position.x ^ transform.position.x <= wayPoint1_1.transform.position.x+.3f)
+                {
+                    transform.position = new Vector2(wayPoint1_1.transform.position.x, transform.position.y);
+                    rb.velocity = new Vector2(0, 0);
+                    climbing = true;
+                }
+            }
+            else
+            {
+                rb.velocity = new Vector2(rb.velocity.x, speed);
+                if (wayPoint1_2.transform.position.y < transform.position.y)
+                {
+                    transform.position = new Vector2(transform.position.x, wayPoint1_2.transform.position.y);
+                    rb.velocity = new Vector2(0, 0);
+                    climbing = false;
+                    currentPoint = 1;
+                }
+            }
+            
+        }
+        if (currentPoint == 1)
+        {
+
+        }
+    }
+}
