34d4716097ac439c51b2834f6beb4800b0bbee4a
Jmat19 <47995875+Jmat19@users.noreply.github.com>
2024-03-21 17:25:17 -0400

    Tilte-screen-added

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class Tutorial : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
+        SceneManager.LoadSceneAsync(2);
     }
 }
